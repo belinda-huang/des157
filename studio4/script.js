a8dde6fe199766c2441886f5dbc8da77aaf4b043
belinda-huang <belindahuang@campus-098-200.ucdavis.edu>
2017-02-14 14:20:38 -0800

    stud4

--- studio4/script.js
+++ studio4/script.js
@@ -3,6 +3,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
+    var countDownDate = new Date("Dec 31, 3000 23:59:59").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById("timeLeft").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timeLeft").innerHTML = "WOW I CAN'T BELIEVE THIS SITE STILL WORKS BUT NOW YOU'VE BEEN TO THE YEAR 3000";
+      }
+    }, 1000);
+
+    /*
     var numberOfDays = document.getElementById('numberOfDays');
     var today = new Date(); // today's date
     var threethou = new Date(3000,0,1); // months are 0-11
@@ -21,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
     numberOfHours.innerHTML = diffHours;
     numberOfMin.innerHTML = diffMin;
     numberOfSec.innerHTML = diffSec;
+    */
 
 
 
