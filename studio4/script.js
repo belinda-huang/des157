53afc067bdd8f683df2a8a2c97ba005e589fcba1
belinda-huang <belindahuang@campus-112-219.ucdavis.edu>
2017-02-16 14:35:59 -0800

    studio 4

--- studio4/script.js
+++ studio4/script.js
@@ -5,31 +5,58 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var countDownDate = new Date("Dec 31, 3000 23:59:59").getTime();
 
-    // Update the count down every 1 second
+    //countdown every second
     var x = setInterval(function() {
 
-      // Get todays date and time
+      //today date time
       var now = new Date().getTime();
 
-      // Find the distance between now an the count down date
+      //calculate distance between date and now
       var distance = countDownDate - now;
 
-      // Time calculations for days, hours, minutes and seconds
+      //when note is selected
+      var elements = document.getElementsByTagName('p');
+      //variable for timed note
+      var noteTimer, noteId;
+
+      //time calculations
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // Display the result in the element with id="demo"
+      //display countdown
       document.getElementById("timeLeft").innerHTML = days + "d " + hours + "h "
       + minutes + "m " + seconds + "s ";
 
-      // If the count down is finished, write some text
+      //IF IT GETS TO YEAR 3000
       if (distance < 0) {
         clearInterval(x);
         document.getElementById("timeLeft").innerHTML = "WOW I CAN'T BELIEVE THIS SITE STILL WORKS BUT NOW YOU'VE BEEN TO THE YEAR 3000";
       }
+
+      //time element for note
+      for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener ('mouseover', function(event){
+        // what if I wanted to send a parameter ('eat') with the call to showMsg?
+        noteId = document.getElementById(this.id);
+        noteTimer = setTimeout(showMsg, 1000);
+      });
+
+      elements[i].addEventListener ('mouseout', function(){
+        clearTimeout(noteTimer);
+        noteId.style.opacity = 0;
+      });
+      }
+
+      function showMsg(){
+        //can I use "this" to find out which object called the function?
+        noteId.style.opacity = 1;
+      }
+
+
     }, 1000);
+
+
 
     /*
     var numberOfDays = document.getElementById('numberOfDays');
